b2b251763fc3bc7993d51dcb3c0b1095b60da5f5
LordCat <kristianalexball@gmail.com>
2023-02-27 00:20:44 +0000

    Trying to get Layout correct. Changed.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <CanvasContainer>
         <TopSection />  
-      <Canvas frames={30}>
+      <Canvas frames={6}>
         <Suspense fallback={null}>
           <Earth />
         </Suspense>
--- src/components/topSection/index.jsx
+++ src/components/topSection/index.jsx
@@ -9,15 +9,20 @@ import { About } from "../../pages/about";
 const TopSectionContainer = styled.div`
   position: absolute;
   width: 100%;
-  height: 100%;
-  background-color: #1756dd32;
+  height: 100%;  
+  z-index: 99;
   display: flex;
   flex-direction: column;
-  z-index: 99;
+  justify-content space-between;
 `;
 
+const ContentContainer = styled.div`
+  display: flex;
+  width: 100%
+  height: auto;
+  flex: 1;
+`;
 const Logo = styled.h1`
-  align-content: right;
   margin: 0;
   height: 50px;
   color: #fff;
@@ -25,15 +30,35 @@ const Logo = styled.h1`
   font-size: 30px;
 `;
 
-const Paragraph = styled.p`
+const SideBar = styled.div`
+  border: 5px solid green;
+  border-radius: 5px;
+
+  width: 20%;
+  height: 100%;
+
   flex: 1;
+`;
+
+const MainBar = styled.div`
+  border: 5px solid green; 
+  border-radius: 5px;
+
+  width: 20%;
+  height: auto;
+
+  flex: 1;
+`;
+
+
+const Paragraph = styled.p`
+  flex: ;
   margin: 0;
   margin-top: 0;
   color: #fff;
   font-size: 14px;
   line-height: 1.5;
   font-weight: 500;
-  max-width: 30%;
   text-align: top;
 `;
 
@@ -42,13 +67,16 @@ const Paragraph = styled.p`
 export function TopSection() {
   return (
     <TopSectionContainer>
-    <BrowserRouter>
-    <NavBar />
+      <BrowserRouter>
+        <NavBar />
+      <MainBar>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
+      </MainBar>
       </BrowserRouter>
+    <SideBar>
       <Logo>Welcome to my Website</Logo>
       <Paragraph>
         This is a website I made to learn React and Three.js and it is taking for ever to correctl render this planet
@@ -56,8 +84,9 @@ export function TopSection() {
         what the issue is. I think it has something to do with the way I am using the react router dom.
         I hope you enjoy
         it!
-
       </Paragraph>
+    </SideBar>
+
     </TopSectionContainer>
   );
 }
